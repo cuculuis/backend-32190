72cc7e1bdf01dd23427723fb40165a350b7262af
cuculuis <luisangel96@gmail.com>
2023-01-25 00:20:04 -0300

    Desafio 10: Añadiendo modelos de plantillas con PUG

---
+++ 10-PugYEJS/PPug/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+const productos = [];
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/productos', (req, res) => {
+    let exist = productos.length > 0;
+    res.render('producto', { productos: productos, listExists: exist});
+    console.log(productos);
+})
+
+app.post('/productos', (req, res) => {
+    productos.push(req.body)
+    res.redirect('/productos')
+})
+
+
+
+
+
+const server = app.listen(PORT, () => {
+    console.log("Servidor escuchando en el puerto: " + PORT);
+})
